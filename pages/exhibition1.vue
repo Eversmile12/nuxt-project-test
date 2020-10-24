89fb2c082395d57e83d785eff6c17fb5dca63d1b
Vittorio Rivabella <20019426@uhi.ac.uk>
2020-10-24 18:53:31 +0200

    initial commit

---
+++ pages/exhibition1.vue
@@ -0,0 +1,19 @@
+<template> 
+  <div>
+    <h1>
+    Exhibition One
+    </h1>
+    <a href="http://localhost:3000/">Ehibit page</a>
+  </div>
+</template>
+
+
+<script>
+  export default{
+  
+  }
+</script>
+
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template> 
+  <div>
+    <h1>
+    Home Page
+    </h1>
+    <a href="exhibition1">Ehibit page</a>
+  </div>
+</template>
+
+
+<script>
+  export default{
+
+  }
+</script>
+
+
+<style>
+</style>
